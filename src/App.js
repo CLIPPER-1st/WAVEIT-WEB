f2f899bd9e4c49d29c070faacdcb980e99635cda
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-02-04 14:34:56 +0900

    feat: 시작페이지 소개글 컴포넌트, 스크롤 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import './App.css';
 import chevronsDown from "./static/Images/chevrons-down.png";
+import Dummy1 from "./static/Images/dummy1.png";
+import React, {useEffect, useRef} from "react";
 const Wrapper=styled.div`
 
   display:grid;
@@ -9,13 +11,13 @@ const Wrapper=styled.div`
 `
 
 const Container=styled.div`
-  flex-direction: column;
-  background-color: #94B6EF;
-  min-height: 100%;
-  display:flex;
-  justify-content:center;
-  tent: center;
-  align-items:center;
+display:flex;
+flex-direction: column;
+background-color: #94B6EF;
+min-width: 100%;
+justify-content:center;
+align-items:center;
+margin: 10% 0% 0%;
 `
 
 const Title=styled.span`
@@ -56,10 +58,34 @@ const NavbarItem=styled.a`
 `
 const Image=styled.img`
   margin: 60px;
-  width: 160px; 
+  width: ${props=>props.width || '160px'}; 
+`
+
+const Description = styled.div`
+  max-width: 260px;
+  & span:first-of-type {
+    font-weight: bold;
+    line-height: 70px;
+    font-size: 20px;
+  }
+`;
+
+
+const DesciptBox=styled.div`
+  min-height: 500px;
+  display:flex;
+  justify-content: space-evenly;
+  align-items:center;
 `
 
 function App() {
+
+  const content1Ref=useRef(null);
+  
+  const onArrowClick=()=>{
+    content1Ref.current?.scrollIntoView({behavior:'smooth'});
+  }
+
   return (
     <Wrapper>
       <Navbar>
@@ -75,9 +101,31 @@ function App() {
       <br/>
       <Title color={"#2519B2"} fontsize={"25px"}>wave-it</Title>
       <Btn>만나러가기</Btn>
-      <Image src={chevronsDown} alt="scroll-arrow" />
+      <Image src={chevronsDown} alt="scroll-arrow" onClick={onArrowClick}/>
     </Container>
-    
+    <DesciptBox>
+      <Description>
+        <span>나에게 맞는 프로젝트</span><br />
+        <span>원하는 조건으로 필터링하여 나에게 맞는 프로젝트만</span>
+      </Description>
+      <Image src={Dummy1} width="350px"/>
+    </DesciptBox>
+    <DesciptBox>
+      <Description  ref={content1Ref}>
+        <span>내가 원하는 팀원과</span><br />
+        <span>모집하고자 하는 분야와 인원, 
+              그리고 어떤 분위기를 추구하는지
+              매칭 등록을 통하여 원하는 팀원과 프로젝트를 시작할 수 있습니다.</span>
+      </Description>
+      <Image src={Dummy1} width="350px" />
+    </DesciptBox>
+    <DesciptBox>
+      <Description>
+        <span>매칭 점수</span><br />
+        <span>과거 팀원의 후기와 인증된 경력으로 팀원의 실력을 확인할 수 있습니다.</span>
+      </Description>
+      <Image src={Dummy1} width="350px" />
+    </DesciptBox>
     </Wrapper>
   );
 }
